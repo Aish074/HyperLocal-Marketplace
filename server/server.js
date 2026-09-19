2cc0eca6624d05ea9b85a455a9ac4c45c89d6351
Aishwarya Kavidayal <kavidayalaishwarya3@gmail.com>
2026-09-19 17:55:34 +0530

    feat: add listings API routes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 import express from "express"
+import listings from "./data/listings.js"
 
 const app = express();
 
-const PORT = 5000;
+const PORT = 8000;
 
 // Middleware
 app.use(express.json());
@@ -11,6 +12,29 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("HyperLocal API is running");
 });
+
+
+
+//get all listings
+app.get("/listings", async(req,res) => {
+  res.json(listings);
+});
+
+// Get one listing
+app.get("/listings/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const listing = listings.find((item) => item.id === id);
+
+  if (!listing) {
+    return res.status(404).json({
+      message: "Listing not found",
+    });
+  }
+
+  res.json(listing);
+});
+
 
 // Start server
 app.listen(PORT, () => {
